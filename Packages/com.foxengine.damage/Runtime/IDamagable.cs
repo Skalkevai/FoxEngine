5e025306183b3eddc1f72153a01d157677eeb726
James Gagnon <39147788+Skalkevai@users.noreply.github.com>
2023-06-27 00:50:47 -0400

    Update IDamagable.cs

--- Packages/com.foxengine.damage/Runtime/IDamagable.cs
+++ Packages/com.foxengine.damage/Runtime/IDamagable.cs
@@ -4,6 +4,6 @@ public interface IDamagable
 {
     public void TakeDamage(float _amount);
 
-    public void Push(Vector3 _force){};
+    public void Push(Vector3 _force){}
     public void Kill();
 }
