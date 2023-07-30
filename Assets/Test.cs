5c54f23c3733c6a61c2ad78aca6c33adfb39ba09
James Gagnon <Skalkevai144@gmail.com>
2023-07-29 21:03:23 -0400

    HideLabel for the Dictionary

---
+++ Assets/Test.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] private FoxDictionary<string,string> foxDictionary = new FoxDictionary<string,string>();
+}
